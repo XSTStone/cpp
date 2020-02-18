37ab63cf606878a4b4014f8880d560a9a55f9cbc
XSTStone <XSTStone@gmail.com>
2020-02-18 11:56:29 +0800

    BrassVirtual

--- BrassVirtual/Brass.cpp
+++ BrassVirtual/Brass.cpp
@@ -7,3 +7,109 @@
 //
 
 #include "Brass.hpp"
+#include <iostream>
+using std::cout;
+using std::endl;
+using std::string;
+
+typedef std::ios_base::fmtflags format;
+typedef std::streamsize precision;
+format setFormat();
+void restore(format f, precision p);
+
+Brass::Brass(const string & s, long an, double bal) {
+    fullName = s;
+    acctNum = an;
+    balance = bal;
+}
+
+void Brass::Deposit(double amt) {
+    if(amt < 0) {
+        cout << "Negative deposit isn't allowed; " << "deposit cancelled.\n";
+    } else {
+        balance += amt;
+    }
+}
+
+void Brass::Withdraw(double amt) {
+    format initialState = setFormat();
+    precision prec = cout.precision(2);
+    
+    if(amt < 0) {
+        cout << "Negative withdraw isn't allowed; " << "withdraw cancelled.\n";
+    } else if(amt <= balance) {
+        balance -= amt;
+    } else {
+        cout << "Withdraw amount of $" << amt << " exceeds your balance.\n";
+        cout << "Withdraw cancelled.\n";
+    }
+    restore(initialState, prec);
+}
+
+double Brass::Balance() const {
+    return balance;
+}
+
+void Brass::ViewAcct() const {
+    format initialState = setFormat();
+    precision prec = cout.precision(2);
+    
+    cout << "Client: " << fullName << endl;
+    cout << "Account Number: " << acctNum << endl;
+    cout << "Balance: $" << balance << endl;
+    restore(initialState, prec);
+}
+
+BrassPlus::BrassPlus(const string & s, long an, double bal, double ml, double r) : Brass(s, an, bal){
+    maxLoan = ml;
+    owesBank = 0.0;
+    rate = r;
+}
+
+BrassPlus::BrassPlus(const Brass & ba, double ml, double r) : Brass(ba){
+    maxLoan = ml;
+    rate = r;
+    owesBank = 0.0;
+}
+
+void BrassPlus::ViewAcct() const {
+    format initialState = setFormat();
+    precision prec = cout.precision(2);
+    
+    Brass::ViewAcct();
+    cout << "MaxLoan: $" << maxLoan << endl;
+    cout << "Owe to bank: $" << owesBank << endl;
+    cout.precision(3);
+    cout << "Loan Rate: $" << rate * 100 << "%" << endl;
+    restore(initialState, prec);
+}
+
+void BrassPlus::Withdraw(double amt) {
+    format initialState = setFormat();
+    precision prec = cout.precision(2);
+    
+    double bal = Balance();
+    if(amt < 0) {
+        cout << "Negative withdraw isn't allowed; withdraw cancelled.\n";
+    } else if(amt <= bal + maxLoan - owesBank) {
+        double advance = amt - bal;
+        owesBank += advance * (1.0 + rate);
+        cout << "Bank advance: $" << advance << endl;
+        cout << "Finance charge: $" << advance * rate << endl;
+        Deposit(advance);
+        Brass::Withdraw(amt);
+    } else {
+        cout << "Credit limit exceeded. Transaction cancelled.\n";
+    }
+    
+    restore(initialState, prec);
+}
+
+format setFormat() {
+    return cout.setf(std::ios_base::fixed, std::ios_base::floatfield);
+}
+
+void restore(format f, precision p) {
+    cout.setf(f, std::ios_base::floatfield);
+    cout.precision(p);
+}
--- BrassVirtual/main.cpp
+++ BrassVirtual/main.cpp
@@ -7,9 +7,54 @@
 //
 
 #include <iostream>
+#include <string>
+#include "Brass.hpp"
+const int CLIENTS = 4;
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    using std::cin;
+    using std::cout;
+    using std::endl;
+    
+    Brass* clientsPointer[CLIENTS];
+    std::string temp;
+    long tempNum;
+    double tempBal;
+    char kind;
+    
+    for (int i = 0;i < CLIENTS;++i) {
+        cout << "Enter client's name: ";
+        getline(cin, temp);
+        cout << "Enter client's account number: ";
+        cin >> tempNum;
+        cout << "Enter opening balance: ";
+        cin >> tempBal;
+        cout << "Enter 1 for Brass Account or 2 for BrassPlus Account: ";
+        while (cin >> kind && (kind != '1' && kind != '2')) {
+            cout << "Enter either 1 or 2: ";
+        }
+        if(kind == '1') {
+            clientsPointer[i] = new Brass(temp, tempNum, tempBal);
+        } else {
+            double tempMax, tempRate;
+            cout << "Enter the overDraft limit: $";
+            cin >>tempMax;
+            cout << "Enter the interest rate as a decimal fraction: ";
+            cin >> tempRate;
+            clientsPointer[i] = new BrassPlus(temp, tempNum, tempBal, tempMax, tempRate);
+        }
+        while (cin.get() != '\n') {
+            continue;
+        }
+    }
+    cout << endl;
+    for (int i = 0;i < CLIENTS;++i) {
+        clientsPointer[i]->ViewAcct();
+        cout << endl;
+    }
+    for (int i = 0;i < CLIENTS;++i) {
+        delete clientsPointer[i];
+    }
+    cout << "Done\n";
     return 0;
 }
