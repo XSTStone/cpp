21165cbafbd08bb522cf34f88f3371557317f8dd
XSTStone <XSTStone@gmail.com>
2020-02-18 09:57:20 +0800

    queue

---
+++ tabTennis/tableTennis.cpp
@@ -0,0 +1,9 @@
+//
+//  tableTennis.cpp
+//  tabTennis
+//
+//  Created by stone on 2020/2/17.
+//  Copyright © 2020 Stone. All rights reserved.
+//
+
+#include "tableTennis.hpp"
---
+++ tabTennis/tableTennis.hpp
@@ -0,0 +1,14 @@
+//
+//  tableTennis.hpp
+//  tabTennis
+//
+//  Created by stone on 2020/2/17.
+//  Copyright © 2020 Stone. All rights reserved.
+//
+
+#ifndef tableTennis_hpp
+#define tableTennis_hpp
+
+#include <stdio.h>
+
+#endif /* tableTennis_hpp */
